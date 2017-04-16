6f8497e852b221277a40ec22bbc7239ee46210c5
Vladislav <vladislav.ykshn@gmail.com>
2017-04-16 22:57:15 +0300

    Everything set and ready.

---
+++ app/src/main/java/lv/android/tsi/langbook/MainActivity.java
@@ -0,0 +1,13 @@
+package lv.android.tsi.langbook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
